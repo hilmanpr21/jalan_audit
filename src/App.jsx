88906010613934a614b27f9ab5aabfec12032932
hilmanpr21 <hilman.prakoso21@gmail.com>
2025-07-02 22:37:25 +0100

    header image change

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
     <div className='app'>
       <header className="header">
         <div className='header-logo'>
-          <img src='src/assets/image/Jalan-logo-05.png' alt="Jalan Logo" className='header-logo'/>
+          <img src='./assets/image/Jalan-logo-05.png' alt="Jalan Logo" className='header-logo'/>
         </div>
         <a href='https://www.instagram.com/kultum.co/'  target='_blank' className='header-logo'>
-          <img src='src/assets/image/Kultum-Visual-04.png' alt="Kultum Logo" className='header-logo'/>
+          <img src='./assets/image/Kultum-Visual-04.png' alt="Kultum Logo" className='header-logo'/>
         </a>
       </header>
       <div className='content-container'>
